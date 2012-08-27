a0b824a7e380f4c19e84f680a30469266c5e1694
Aleksey <sh-a-n@yandex.ru>
2012-08-27 11:18:09 +0400

    Initial Commit

---
+++ русский как иностранный/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  русский как иностранный
+//
+//  Created by Lion User on 27.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
